0f8f84b502d5fb16d9cc7732117f6f49f3fc10d3
DESKTOP-BAFHRBB\c <wkdghk24@gmail.com>
2022-12-02 00:15:39 +0900

    2022 12 02 - factory 연습
    factory 연습을 위해서 interface를 생성하고,
    interface를 구현하는 class,
    구현한 class의 객체를 생성하는 factory,
    factory를 이용하여 값을 얻는 class를 생성했습니다.

---
+++ src/main/java/org/example/factory_exercise/Circle.java
@@ -0,0 +1,8 @@
+package org.example.factory_exercise;
+
+public class Circle implements Shape{
+    @Override
+    public void draw() {
+        System.out.println("it's Circle");
+    }
+}
---
+++ src/main/java/org/example/factory_exercise/Square.java
@@ -0,0 +1,8 @@
+package org.example.factory_exercise;
+
+public class Square implements Shape{
+    @Override
+    public void draw() {
+        System.out.println("It'square");
+    }
+}
